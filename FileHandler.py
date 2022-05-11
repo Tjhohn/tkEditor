192f90d4d59ab8df9c6da8c70305a363d21835c1
Tjhohn <tannerhohn@gmail.com>
2022-05-11 11:29:42 -0600

    updated file options

--- FileHandler.py
+++ FileHandler.py
@@ -9,10 +9,12 @@ class FileHandler:
     # Initializer
     def __init__(self, pane):
         self._pane = pane
+        self._allowed_file_types = [("Python files", "*.py *.pyw", "TEXT"), ("Text files", "*.txt", "TEXT"),
+                                    ("C files", "*.c *.cpp *.h *.hpp", "TEXT"), ("All files", "*"), ]
 
     # open a few tab and fills tab with contents of the file, may only allow .py files currently?
     def open_file(self):
-        file_path = askopenfilename(filetypes=[('Python Files', '*.py')])
+        file_path = askopenfilename(filetypes=self._allowed_file_types)
 
         if file_path != '':
             with open(file_path, 'r') as file:
@@ -20,7 +22,7 @@ class FileHandler:
                 self._pane.add_editor_tab(name=ntpath.basename(file_path), file_content=code, file_path=file_path)
 
     def save_as(self, current_tab):  # also npw all files are .py
-        file_path = asksaveasfilename(filetypes=[('Python Files', '*.py')], defaultextension='*.*')
+        file_path = asksaveasfilename(filetypes=self._allowed_file_types, defaultextension='*.*')
 
         if file_path != '':
             self._pane.rename_current_editor(ntpath.basename(file_path))
